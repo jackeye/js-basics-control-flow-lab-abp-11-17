708c2c5d480ea6818b110583cb3ac4118b83052c
Jackie Baxter <jackeye@gmail.com>
2017-11-14 01:57:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-function scuberGreetingForFeet(ride) {
-  if (ride <= 400) {
-      return 'This one is on me!';
-    } else if (ride >= 2000) {
-      return 'I will gladly take your thirty bucks.'
-    } else if (ride > 2500) {
-      return 'No can do.'
-    }
+
+function scuberGreetingForFeet (ride){
+  if (ride <= 400){
+    return 'This one is on me!'
+  } else if (ride > 2000 && ride < 2501){
+    return 'I will gladly take your thirty bucks.'
+  } else if (ride > 2500) {
+    return 'No can do.'
+  }
 }
-
-//  return 'I will gladly take your thirty bucks.';
-//}
-
-
-
